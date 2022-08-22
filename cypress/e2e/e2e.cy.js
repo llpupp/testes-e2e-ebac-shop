9bd468d2605146fa92f0dff8452cdc0073991cbe
Luciano Pupp <llpupp@hotmail.com>
2022-08-22 14:16:23 -0300

    Alteração timeout

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -99,7 +99,7 @@ describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
         cy.get('#terms').check()
         cy.get('#place_order').click({ force: true })
-        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+        cy.get('.woocommerce-notice', {timeout:10000}).should ('be.visible').should('contain', 'Obrigado. Seu pedido foi recebido.')
     });
 
 
